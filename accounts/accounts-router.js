ca9be9684181d825e776c8c4a495285416574e20
royeradames <royeraadames@gmail.com>
2020-09-06 22:41:26 -0400

    update method is completed and tested

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -9,47 +9,65 @@ const router = express.Router()
 // done
 router.get('/', (req, res) => {
     db('accounts')
-        .then( accounts => {
-            res.status(200).json({data:accounts})
+        .then(accounts => {
+            res.status(200).json({ data: accounts })
         })
         .catch(error => {
             console.log(error)
-    
+
             res.status(500).json({ error: error.message })
         })
 })
 // done
 router.get("/:id", (req, res, next) => {
     db('accounts')
-    .where('id', req.params.id)
-    .then( accounts => {
-        res.status(200).json({data:accounts})
-    })
-    .catch(error => {
-        console.log(error)
+        .where('id', req.params.id)
+        .then(accounts => {
+            res.status(200).json({ data: accounts })
+        })
+        .catch(error => {
+            console.log(error)
 
-        res.status(500).json({ error: error.message })
-    })
+            res.status(500).json({ error: error.message })
+        })
 })
 // done
 router.post("/", (req, res, next) => {
     const newAccount = req.body
     db('accounts')
-    .insert(newAccount)
-    .returning('id')//do not exlude this line if you plan to support PostgreSQL
-    .then( ids => {
-        res.status(200).json({data:ids})
-    })
-    .catch(error => {
-        console.log(error)
+        .insert(newAccount)
+        .returning('id')//do not exlude this line if you plan to support PostgreSQL
+        .then(ids => {
+            res.status(200).json({ data: ids })
+        })
+        .catch(error => {
+            console.log(error)
 
-        res.status(500).json({ error: error.message })
-    })
+            res.status(500).json({ error: error.message })
+        })
 })
+// done
 router.put("/:id", (req, res, next) => {
+    const changes = req.body
 
+    db('accounts')
+        .where('id', req.params.id)
+        .update(changes)
+        .then(count => {
+            if (count)
+                res.status(200).json({ message: 'updated succesfully' })
+            else
+                res.status(500).json({ error: error.message })
+
+        })
+        .catch(error => {
+            console.log(error)
+
+            res.status(500).json({ error: error.message })
+        })
 })
-router.delete("/:id", (req, res, next) => { 
+
+router.delete("/:id", (req, res, next) => {
 
 })
 
